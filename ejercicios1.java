c0b8a3557cd1bc8da0354753d9fb5fbabc918b10
Dannyuu <danielabu1901@gmail.com>
2024-08-25 18:43:02 -0500

    Add files via upload

---
+++ ejercicios1.java
@@ -0,0 +1,16 @@
+package lab1;
+import java.util.*;
+public class ejercicios1 {
+	public static int sumaArreglo(int[] arreglo) {
+		int suma = 0;
+		for (int i = 0; i < arreglo.length; i++) {
+			suma += arreglo[i];
+		}
+		return suma;
+	}
+	public static void main(String[] args) {
+		int[] arreglo = {1, 2, 3, 4, 5};
+		int suma = sumaArreglo(arreglo);
+		System.out.println("Suma del arreglo: " + suma);
+	}
+}
